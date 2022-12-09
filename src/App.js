9233a95fff5fde8041359aac12a1d9feaa8c51bc
Matthew Arofin <Matt.arofin@gmail.com>
2022-12-09 18:19:36 -0500

    added route scaffolding to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./views/global/Topbar";
+import Sidebar from "./views/global/Sidebar";
+import Dashboard from "./views/dashboard";
+// import Team from "./views/team";
+// import Invoices from "./views/invoices";
+// import Contacts from "./views/contacts";
+// import Bar from "./views/far";
+// import Form from "./views/form";
+// import Line from "./views/line";
+// import Pie from "./views/pie";
+// import FAQ from "./views/faq";
+// import Geography from "./views/geography";
+// import Calendar from "./views/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,8 +23,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
