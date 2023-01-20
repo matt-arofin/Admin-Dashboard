7082e8f3bc22cec8c033d50903ad38494e0741af
Matthew Arofin <Matt.arofin@gmail.com>
2023-01-20 16:23:08 +0100

    Implemented PieChart oage. Bug does not render this component on-screen and will need fixing

--- src/views/calendar/index.jsx
+++ src/views/calendar/index.jsx
@@ -38,77 +38,79 @@ const Calendar = () => {
         }
     };
 
-    return <Box m="20px">
-        <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+    return (
+        <Box m="20px">
+            <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
 
-        <Box display="flex" justifyContent="space-between">
-            {/* calendar sidebar */}
-            <Box
-                flex="1 1 20%" 
-                backgroundColor={colors.primary[400]} 
-                p="15px" 
-                borderRadius="4px"
-            >
-                <Typography variant="h5">Events</Typography>
-                <List>
-                    {currentEvents.map((event) => {
-                        <ListItem
-                            key={event.id}
-                            sx={{
-                                backgroundColor: colors.greenAccent[500],
-                                margin: "10px 0",
-                                borderRadius: "2px"
+            <Box display="flex" justifyContent="space-between">
+                {/* calendar sidebar */}
+                <Box
+                    flex="1 1 20%" 
+                    backgroundColor={colors.primary[400]} 
+                    p="15px" 
+                    borderRadius="4px"
+                >
+                    <Typography variant="h5">Events</Typography>
+                    <List>
+                        {currentEvents.map((event) => {
+                            <ListItem
+                                key={event.id}
+                                sx={{
+                                    backgroundColor: colors.greenAccent[500],
+                                    margin: "10px 0",
+                                    borderRadius: "2px"
+                                }}
+                            >
+                                <ListItemText
+                                    primary={event.title}
+                                    secondary={
+                                        <Typography>
+                                            {formatDate(event.start, {
+                                                year: "numeric",
+                                                month: "short",
+                                                day: "numeric"
+                                            })}
+                                        </Typography>
+                                    }
+                                />
+                            </ListItem>
+                        })}
+                    </List>
+                </Box>
+
+                {/* Calendar */}
+                <Box flex="1 1 100%" ml="15px">
+                        <FullCalendar
+                            height="75vh"
+                            plugins={[
+                                dayGridPlugin,
+                                timeGridPlugin,
+                                interactionPlugin,
+                                listPlugin
+                            ]}
+                            headerToolbar={{
+                                left: "prev, next, today",
+                                center: "title",
+                                right: "dayGridMonth, timeGridWeek, timeGridDay, listMonth"
                             }}
+                            initialView="dayGridMonth"
+                            editable={true}
+                            selectable={true}
+                            selectMirror-={true}
+                            dayMaxEvents={true}
+                            select={handleDateClick}
+                            eventClick={handleEventClick}
+                            eventsSet={(events) => setCurrentEvents(events)}
+                            initialEvents={[
+                                { id:"1234", title: "All-day event", date: "2023-01-14"},
+                                { id:"4321", title: "Timed event", date: "2023-01-28"}
+                            ]}
                         >
-                            <ListItemText
-                                primary={event.title}
-                                secondary={
-                                    <Typography>
-                                        {formatDate(event.start, {
-                                            year: "numeric",
-                                            month: "short",
-                                            day: "numeric"
-                                        })}
-                                    </Typography>
-                                }
-                            />
-                        </ListItem>
-                    })}
-                </List>
-            </Box>
-
-            {/* Calendar */}
-            <Box flex="1 1 100%" ml="15px">
-                    <FullCalendar
-                        height="75vh"
-                        plugins={[
-                            dayGridPlugin,
-                            timeGridPlugin,
-                            interactionPlugin,
-                            listPlugin
-                        ]}
-                        headerToolbar={{
-                            left: "prev, next, today",
-                            center: "title",
-                            right: "dayGridMonth, timeGridWeek, timeGridDay, listMonth"
-                        }}
-                        initialView="dayGridMonth"
-                        editable={true}
-                        selectable={true}
-                        selectMirror-={true}
-                        dayMaxEvents={true}
-                        select={handleDateClick}
-                        eventClick={handleEventClick}
-                        eventsSet={(events) => setCurrentEvents(events)}
-                        initialEvents={[
-                            { id:"1234", title: "All-day event", date: "2023-01-14"},
-                            { id:"4321", title: "Timed event", date: "2023-01-28"}
-                        ]}
-                    >
-                    </FullCalendar>
+                        </FullCalendar>
+                </Box>
             </Box>
         </Box>
-    </Box>
+    )
 }
 
 export default Calendar;
